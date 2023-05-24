e956c5b215b73ddd8afb11c645e94dc0a5360847
Kalle <kalle.voutilainen@metropolia.fi>
2023-05-24 19:12:38 +0300

    Changing background image depending on the current weather type and more weather data on display.

--- weather-app/src/Components/WeatherDisplay/WeatherDisplay.js
+++ weather-app/src/Components/WeatherDisplay/WeatherDisplay.js
@@ -10,12 +10,30 @@ const WeatherDisplay = ({currentWeatherInfo, cityName}) => {
         }
         return null;
     }
+
+
     return (
         <>
             <h1 className={'city-name'}>{cityName}</h1>
-            <div className='weather-icon-and-current-temp'>
-                {findWeatherIcon() ? (<img className={'weather-icon'} src={findWeatherIcon()} alt='Weather icon'/>) : null}
+            <div className={'weather-icon-and-current-temp'}>
+                {findWeatherIcon() ? (
+                    <img className={'weather-icon'} src={findWeatherIcon()} alt='Weather icon'/>) : null}
                 <p className={'current-temp'}>{Math.round(currentWeatherInfo.main.temp)}°C</p>
+            </div>
+            <div className={'current-weather'}>
+                <h3 className={'weather-stats'}>Feels like</h3>
+                <h3 className={'weather-stats'}>Wind speed</h3>
+                <h3 className={'weather-stats'}>Visibility</h3>
+                <h3 className={'weather-stats'}>Humidity</h3>
+                <h3 className={'weather-stats'}>Lowest temp. today</h3>
+                <h3 className={'weather-stats'}>Highest temp. today</h3>
+
+                <p className={'weather-stat-numbers'}>{Math.round(currentWeatherInfo.main.feels_like)} °C</p>
+                <p className={'weather-stat-numbers'}>{currentWeatherInfo.wind.speed} m/s</p>
+                <p className={'weather-stat-numbers'}>{currentWeatherInfo.visibility} m</p>
+                <p className={'weather-stat-numbers'}>{currentWeatherInfo.main.humidity} %</p>
+                <p className={'weather-stat-numbers'}>{Math.round(currentWeatherInfo.main.temp_min)} °C</p>
+                <p className={'weather-stat-numbers'}>{Math.round(currentWeatherInfo.main.temp_max)} °C</p>
             </div>
         </>
     );
